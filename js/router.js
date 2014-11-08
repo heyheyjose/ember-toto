159e576f653b8ba3d79bfd5b6d7e9712741a6a3e
heyheyjose <heyheyjose@users.noreply.github.com>
2014-11-07 22:54:55 -0500

    router updated with new route to show active todos only + index updated

--- js/router.js
+++ js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function () {
 	this.resource('todos', { path: '/' }, function () {
 		// additional child routes will be put here later
+		this.route('active');
 	});
 });
 
@@ -16,6 +17,20 @@ Todos.TodosIndexRoute = Ember.Route.extend({
 
 	model: function () {
 		return this.modelFor('todos');
+	}
+
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+
+	model: function () {
+		return this.store.filter('todo', function (todo) {
+			return !todo.get('isCompleted');
+		});
+	},
+
+	renderTemplate: function (controller) {
+		this.render('todos/index', {controller: controller});
 	}
 
 });
