144994e93b0eb3a65cffd468d409abcaebc0ba01
heyheyjose <heyheyjose@users.noreply.github.com>
2014-11-07 20:42:35 -0500

    added todos controller + updated index so that new instance of model (todo) can be created

---
+++ js/controllers/todos_controller.js
@@ -0,0 +1,22 @@
+Todos.TodosController = Ember.ArrayController.extend({
+	actions: {
+		createTodo: function () {
+			// get the todo title set by the 'new todo' text field
+			var title = this.get('newTitle');
+			if (!title) { return false; }
+			if (!title.trim()) { return; }
+
+			// create the new Todo model
+			var todo = this.store.createRecord('todo', {
+				title: title,
+				isCompleted: false
+			});
+
+			// clear the 'new todo' text field
+			this.set('newTitle', '');
+
+			// save the new model
+			todo.save();
+		}
+	}
+});
