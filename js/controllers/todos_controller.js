6ae430a9642dcf2c0c71421411dbde1602f9353b
heyheyjose <heyheyjose@users.noreply.github.com>
2014-11-07 23:29:53 -0500

    button to remove all completed todos is functional now

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,6 +1,11 @@
 Todos.TodosController = Ember.ArrayController.extend({
 
 	actions: {
+		clearCompleted: function () {
+			var completed = this.filterBy('isCompleted', true);
+			completed.invoke('deleteRecord');
+			completed.invoke('save');
+		},
 		createTodo: function () {
 			// get the todo title set by the 'new todo' text field
 			var title = this.get('newTitle');
@@ -21,6 +26,14 @@ Todos.TodosController = Ember.ArrayController.extend({
 		}
 	},
 
+	hasCompleted: function () {
+		return this.get('completed') > 0;
+	}.property('completed'),
+
+	completed: function () {
+		return this.filterBy('isCompleted', true).get('length');
+	}.property('@each.isCompleted'),
+
 	remaining: function () {
 		return this.filterBy('isCompleted', false).get('length');
 	}.property('@each.isCompleted'),
@@ -29,5 +42,5 @@ Todos.TodosController = Ember.ArrayController.extend({
 		var remaining = this.get('remaining');
 		return remaining === 1 ? 'item' : 'items';
 	}.property('remaining')
-	
+
 });
